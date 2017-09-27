c2bb187b0f9be25819a9ac608dc8d1b668026752
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-09-27 14:31:26 -0700

    Setup boilerplate and import apollo to interface with stanza api

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo';
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://www.stanza.dance/api/'
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+);
+registerServiceWorker();
